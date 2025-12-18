c55fd0afe8b650ca5e4e68473e5908784ee14efd
guilherme6henriques <guilherme6henriques@gmail.com>
2025-12-18 12:55:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -130,7 +130,7 @@ const data = {
         rooms: [
           { id: "1Q", code: "AR1A1Q", label: { en: "Room 1 - NOT AVAILABLE", pt: "Quarto 1 - INDISPONÍVEL"}, price: 550,
             bills: { en: "All bills included", pt: "Todas as contas incluídas" },
-            thumb: `Images/AR1A1Q1F.jpeg`, photos: getImagePaths2("AR1A1Q", 4),
+            thumb: `Images/AR1A1Q1F.jpg`, photos: getImagePaths("AR1A1Q", 2),
             description: { en: "", pt: "" }, availableFrom: "01/01/2027" },
           { id: "2Q", code: "AR1A2Q", label: { en: "Room 2", pt: "Quarto 2" }, price: 650,
             bills: { en: "All bills included", pt: "Todas as contas incluídas" },
@@ -176,6 +176,10 @@ const data = {
         },
         commonPhotos: getImagePaths("AR3AZC", 5),
         rooms: [
+          { id: "1Q", code: "AR3A1Q", label: { en: "Room 1", pt: "Quarto 1"}, price: 550,
+            bills: { en: "All bills included", pt: "Todas as contas incluídas" },
+            thumb: `Images/AR3A1Q1F.jpeg`, photos: getImagePaths2("AR3A1Q", 5),
+            description: { en: "", pt: "" }, availableFrom: "01/03/2026" },
           { id: "2Q", code: "AR3A2Q", label: { en: "Room 2", pt: "Quarto 2" }, price: 650,
             bills: { en: "All bills included", pt: "Todas as contas incluídas" },
             thumb: `Images/AR3A2Q1F.jpg`, photos: getImagePaths("AR3A2Q", 6),
@@ -502,6 +506,7 @@ document.addEventListener("keydown", (e) => {
     imageModal.classList.add("hidden");
   }
 });
+
 
 
 
