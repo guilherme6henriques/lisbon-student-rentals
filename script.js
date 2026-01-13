6b1d5d032b2fb10ca8010f17b9a24a38715ef6ac
guilherme6henriques <guilherme6henriques@gmail.com>
2026-01-13 16:28:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -230,7 +230,7 @@ const data = {
             bills: { en: "All bills included (excluding gas) <br> The room includes a pillow, a duvet, bed sheets, and a towel. <br> It is lockable for your privacy.", pt: "Contas incluídas (gás excluído). <br> O quarto inclui uma almofada, um edredão, lençóis de cama e uma toalha. <br> Tem fechadura para sua privacidade." },
             thumb: `Images/AL1A3Q1F.jpg`, photos: getImagePaths("AL1A3Q", 4),
             description: { en: "", pt: "" }, availableFrom: "01/07/2026" },
-          { id: "4Q", code: "AL1A4Q", label: { en: "Room 4", pt: "Quarto 4" }, price: 600,
+          { id: "4Q", code: "AL1A4Q", label: { en: "Room 4 - NOT AVAILABLE", pt: "Quarto 4 - INDISPONÍVEL" }, price: 600,
             bills: { en: "All bills included (excluding gas) <br> The room includes a pillow, a duvet, bed sheets, and a towel. <br> It is lockable for your privacy.", pt: "Contas incluídas (gás excluído). <br> O quarto inclui uma almofada, um edredão, lençóis de cama e uma toalha. <br> Tem fechadura para sua privacidade." },
             thumb: `Images/AL1A4Q1F.jpg`, photos: getImagePaths("AL1A4Q", 5),
             description: { en: "", pt: "" }, availableFrom: "15/01/2026" }
@@ -523,6 +523,7 @@ document.addEventListener("keydown", (e) => {
     imageModal.classList.add("hidden");
   }
 });
+
 
 
 
